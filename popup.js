fbedbf63d7ffbed0c88063aa9d860e7ac2f4bd2d
JanBlazejczyk <jan.blazejczyk@allegro.pl>
2022-11-12 01:10:26 +0100

    TAB | tab closing  functionality added

--- popup.js
+++ popup.js
@@ -1,24 +1,22 @@
 const tabsList = document.querySelector('.tabs-list');
 
-const closeDuplicates = (tabUrl) => {
-    chrome.tabs.query({}).then((tabs) => {
-        const sameAddressTabsIds = tabs.filter(tab => tab.url === tabUrl).map(tab => tab.id);
-        // lave the one that is active if there is one if not return everything except one
-        const tabsToClose = sameAddressTabsIds.slice(1); 
+const closeDuplicates = (url) => {
+    chrome.tabs.query({ url }).then((tabs) => {
+        // active tab cannot be on the list
+        const activeTab = tabs.find(tab => tab.active);
+        const tabsToClose = activeTab ? tabs.filter(tab => !tab.active).map(tab => tab.id) : tabs.slice(1).map(tab => tab.id); 
         chrome.tabs.remove(tabsToClose);
+        location.reload();
     })
 };
 
 chrome.storage.local.get('duplicatedTabs', ({ duplicatedTabs }) => {
-    // create an iterable and list all the tabs currently opened in the browser
     duplicatedTabs.forEach((tab) => {
-        console.log(tab);
         const listItem = document.createElement('li');
         listItem.classList.add('tabs-list__item');
         listItem.innerHTML = `<span>${tab.title}</span><button id=${tab.id}>x</button>`
         tabsList.appendChild(listItem);
         const button = document.getElementById(`${tab.id}`);
-        // better to close the tab that is opened before
         button.addEventListener('click', () => closeDuplicates(tab.url));
     });
 });
